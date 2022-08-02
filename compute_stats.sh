dec82ab53879299b99681c59b1a798cc75a6c231
Will Dumm <wrhdumm@gmail.com>
2022-08-02 14:04:06 -0700

    add dag stats summary scripts

---
+++ compute_stats.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+set -eu
+
+# NOTE: Might be unnecessary
+# # Init shell
+# conda list
+eval "$(conda shell.bash hook)"
+conda activate build_usher_trees
+
+for CLADEDIR in clades/*/; do
+    sbatch -c 1 -J $(basename $CLADEDIR) -o $CLADEDIR/stats.log ./single_summary.sh $CLADEDIR
+done
+
+while :
+do
+    sleep 40
+    NJOBS=$(squeue -l -u $USER | wc -l)
+    [ $NJOBS == 2 ] && break
+done
+
+# Now all jobs are finished!
+Cladepattern="clades/*/"
+CLADES=( $Cladepattern )
+head -1 $(echo $CLADES)/stats.csv > all_stats.csv
+for CLADEDIR in clades/*/; do
+    tail -n 1 $CLADEDIR/stats.csv >> all_stats.csv || true
+
